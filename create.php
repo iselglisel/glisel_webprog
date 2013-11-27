ced0aec21d40cf686a85f84b4601e1a5716540e9
iselglisel <nice_glisel@yahoo.com.ph>
2013-11-27 08:52:35 +0800

    Create create.php

---
+++ create.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+	<title>Post Messages</title>
+</head>
+
+
+<body><center><br><br><br><br><br><br>
+	<font face="Century Gothic" size="5">POST MESSAGES</font>
+	<form method="POST" action="ViewMessages.php">
+			Name : <input type="text" size="20" name="name"><br>
+			Email : <input type="text" size="20" name="email"><br><br>
+			Message : <textarea name="messages" name="messages"></textarea>
+				<input type="submit" values="POST MESSAGES">
+		
+	</center>		
+	</form>
+</body>
+</html>
